b6fa2a8e047c8ab267553dc05eea515b25a84de7
muktabehera <mukta.behera@gmail.com>
2017-08-23 13:28:43 -0700

    415 {"requestErrors":[{"message":"Cannot consume content type","code":"unknown","ref":null,"data":null}],"parameterErrors":[]} Test Case GetAuthorizationSystems: Fail

---
+++ Authorization Systems/DeleteAuthorizationSystemsByID.py
@@ -0,0 +1,20 @@
+import requests
+
+test_case = 'DeleteAuthorizationSystemsByID'
+url = 'http://172.30.3.174/authorizationSystems/222'
+Token = 'eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJkZWZhdWx0Iiwic3ViIjoiZGVsaXZlcnlAdG9rZW4uY29tIiwiYXVkIjoicWVkOmRlZmF1bHQiLCJxZWRwIjpbImQiLCJnIiwiYyIsInMiLCJtIiwiYSIsInAiXSwiZXhwIjoxNTY3MTk5ODQ4fQ.SLo2AoCFsj9awqYxG7u5UwC6QZD4S9IlE1XMOuOKNGo'
+header = {
+"Accept": "application/json,application/vnd.qumu.qed+json,application/vnd.qumu.qed.v1+json" ,
+"Authorization": "Bearer "+Token+""
+}
+#print header
+response1 = requests.delete(url,headers=header)
+response2 = requests.get(url,headers=header)
+# print response.status_code
+#print response.text
+
+
+if response1.status_code == 200 and response2.status_code == 404 :
+    print 'Test Case '+test_case+': Pass'
+else:
+    print 'Test Case ' + test_case + ': Fail'
