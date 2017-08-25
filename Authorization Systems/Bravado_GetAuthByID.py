5b9f81ca131fc29c08cfc8d555346f1a44004021
muktabehera <mukta.behera@gmail.com>
2017-08-25 11:25:09 -0700

    pytest

--- Authorization Systems/Bravado_GetAuthByID.py
+++ Authorization Systems/Bravado_GetAuthByID.py
@@ -2,17 +2,30 @@ from bravado.requests_client import RequestsClient
 from bravado.client import SwaggerClient
 
 http_client = RequestsClient()
-url = 'http://172.30.4.216/content/apidocs/index.html'
-token = 'eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJkZWZhdWx0Iiwic3ViIjoiYWRtaW5AMTcyLjMwLjQuMjE2LmNvbSIsImF1ZCI6InFlZDpkZWZhdWx0IiwicWVkcCI6WyJzIiwiYyIsImciLCJwIiwiZCIsIm0iLCJhIl19.3NzYb3lS8W3WMNBDRS0q5Zoh3pZJxWRUaXjvyszEsvs'
-header1 = {
-"Accept": "application/json,application/vnd.qumu.qed+json,application/vnd.qumu.qed.v1+json" ,
-"Authorization": "Bearer "+token+""
-}
+url = 'http://172.30.3.174/content/apidocs/index.html'
+token = 'eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJkZWZhdWx0Iiwic3ViIjoiZGVsaXZlcnlAdG9rZW4uY29tIiwiYXVkIjoicWVkOmRlZmF1bHQiLCJxZWRwIjpbImQiLCJnIiwiYyIsInMiLCJtIiwiYSIsInAiXSwiZXhwIjoxNTY3MTk5ODQ4fQ.SLo2AoCFsj9awqYxG7u5UwC6QZD4S9IlE1XMOuOKNGo'
+# header1 = {
+# "Accept": "application/json,application/vnd.qumu.qed+json,application/vnd.qumu.qed.v1+json" ,
+# "Authorization": "Bearer "+token+""
+# }
 
 http_client.set_api_key(
     'url',
-    'token', param_in='header1'
+    'token'
 )
-client = SwaggerClient.from_url('http://172.30.4.216/services/apidocs/qed.json', http_client=http_client)
-# AuthorizationSystemsByID = client.AuthorizationSystems.authorizationSystemsById(authorizationSystemsid= 'default').result()
-print header1
+client = SwaggerClient.from_url('http://172.30.3.174/services/apidocs/qed.json', http_client=http_client)
+AuthorizationSystemsByID = client.AuthorizationSystems.authorizationSystemsById(authorizationSystemsid= 'default').result()
+
+# from bravado.requests_client import RequestsClient
+# from bravado.client import SwaggerClient
+#
+# http_client = RequestsClient()
+# http_client.set_api_key(
+#     'http://petstore.swagger.io', 'special-key'
+# )
+# client = SwaggerClient.from_url(
+#     'http://petstore.swagger.io/v2/swagger.json',
+#     http_client=http_client,
+# )
+# pet = client.pet.getPetById(petId=42).result()
+# print pet
